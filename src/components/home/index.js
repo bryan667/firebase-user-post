b259fb999f60db9cc1a537ac6f7f25389a2fd36a
Bryan M <janbryanmartirez@gmail.com>
2019-01-08 16:18:50 +0800

    home css

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -6,47 +6,56 @@ import '../../css/home.css'
 
 const Home = ({user, userData})=> {
     return (
-        <div className='home_main'>
-            <div className='home_container'>
+        <div className='home_main' >
                 {user!=null ? 
-                    <React.Fragment>
-                        <div className='image'>
-                            <div className='image_wrapper'>
-                            {userData.imageURL ?
-                                <img src={userData.imageURL} alt='awyis'></img>
-                            :
-                                <div className='spinner_wrapper'>
-                                    <ReactLoading 
-                                    className='spinner'
-                                    type={'spin'} 
-                                    color={'blue'} 
-                                    height={'10%'} 
-                                    width={'10%'} />
+                <div>
+                    <div className='home_container'>
+                            {userData.firstName!=='' ?
+                            <React.Fragment>
+                                <div className='background'>
                                 </div>
-                            }
+                                <div className='image_div'>
+                                    <div className='image_wrapper'>
+                                        <img src={userData.imageURL} alt='awyis'></img>
+                                    </div>
+                                </div>
+                                <div className='info'>
+                                    <h2>{`Welcome ${userData.firstName} ${userData.lastName}!`}</h2>           
+                                    <div>{`logged in as ${userData.email}`}</div>
+                                </div>
+
+                                <div className='intro'>
+                                    <div>
+                                        <h1>Intro</h1>
+                                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                                    </div>
+                                </div>
+
+                                <div className='description'>
+                                    <div>
+                                        <h1>About Me</h1>
+                                        <p>""Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?""</p>
+                                    </div>
+                                </div>
+                            </React.Fragment>
+                            : 
+                            <div className='spinner_wrapper'>
+                                <ReactLoading 
+                                className='spinner'
+                                type={'spin'} 
+                                color={'blue'} 
+                                height={'10%'} 
+                                width={'10%'} />
                             </div>
-                        </div>
-                        <div className='info'>
-                            <div>{`Welcome ${userData.firstName} ${userData.lastName}!`}</div>           
-                            <div>{`logged in as ${userData.email}`}</div>
-                        </div>
-
-                        <div className='intro'>
-                            <h1>Intro</h1>
-                            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                        </div>
-
-                        <div className='description'>
-                            <h1>Description</h1>
-                            <p>""Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?""</p>
-                        </div>
-                    </React.Fragment>
+                            }
+                    </div>
+                </div>
                 :
                     <div>
                         Please <Link to={`/sign_in`}>sign In</Link>
                     </div>
+                    
                 }
-            </div>
         </div>
     );
 }
